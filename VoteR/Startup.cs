09f584767a86c57ed576f297f7de7183c1f64305
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2014-08-30 21:37:41 +0000

    initial commit with code directly extracted from SignalR sample code

---
+++ VoteR/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace VoteR
+{
+    public class Startup
+    {
+
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void Configure(IBuilder app)
+        {
+            app.UseServices(services =>
+            {
+                services
+                    .AddSignalR()
+                    .SetupOptions(options =>
+                    {
+                        options.Hubs.EnableDetailedErrors = true;
+                    });
+            });
+
+            app.UseFileServer();
+
+            app.UseSignalR<RawConnection>("/raw-connection");
+            app.UseSignalR();
+        }
+
+    }
+}
